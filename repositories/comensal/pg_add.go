30944734c53c4b991f6a903dea123cd10798d8d5
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-11-05 23:42:40 -0500

    Comensal_basic a Comensal

--- repositories/comensal/pg_add.go
+++ repositories/comensal/pg_add.go
@@ -7,7 +7,7 @@ import (
 func Pg_Add_BasicData(comensalpg models.Pg_Comensal) error {
 
 	db := models.Conectar_Pg_DB()
-	q := "INSERT INTO Comensal_BasicData(idcountry,name,lastname, phone,password,createddate,updateddate) VALUES ($1,$2,$3,$4,$5,$6,$7)"
+	q := "INSERT INTO Comensal(idcountry,name,lastname, phone,password,createddate,updateddate) VALUES ($1,$2,$3,$4,$5,$6,$7)"
 	add_checker, err_add := db.Prepare(q)
 
 	if err_add != nil {
