a4bb8bf1728139b22e62be9dfedccaf9522d7379
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-11-05 22:58:25 -0500

    Agregue name y latname en add

--- repositories/comensal/pg_add.go
+++ repositories/comensal/pg_add.go
@@ -7,13 +7,13 @@ import (
 func Pg_Add_BasicData(comensalpg models.Pg_Comensal) error {
 
 	db := models.Conectar_Pg_DB()
-	q := "INSERT INTO Comensal_BasicData(idcountry,phone,password,createddate,updateddate) VALUES ($1,$2,$3,$4,$5)"
+	q := "INSERT INTO Comensal_BasicData(idcountry,name,lastname, phone,password,createddate,updateddate) VALUES ($1,$2,$3,$4,$5,$6,$7)"
 	add_checker, err_add := db.Prepare(q)
 
 	if err_add != nil {
 		return err_add
 	}
 
-	add_checker.Exec(comensalpg.IdCountry, comensalpg.Phone, comensalpg.Password, comensalpg.UpdatedDate, comensalpg.UpdatedDate)
+	add_checker.Exec(comensalpg.IdCountry, comensalpg.Name, comensalpg.LastName, comensalpg.Phone, comensalpg.Password, comensalpg.UpdatedDate, comensalpg.UpdatedDate)
 	return nil
 }
